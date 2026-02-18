7f78c5d7c2ce4952b5fc7cef6304dede881db9da
Seungjoo Yoon <44917141+sngjyoon@users.noreply.github.com>
2026-02-18 12:34:52 -0500

    updated phishing.py

--- phishing_data.py
+++ phishing_data.py
@@ -1,7 +1,24 @@
+from transformers import DistilBertTokenizerFast
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
 file_path = "/Users/seungjooyoon/local/code/github/cloned/PhishingSpamDataSet/4_MergeDataSet/merged_all_emails.csv"
 
 df = pd.read_csv(file_path, index_col=0) # (12,300, 12)
 #df_body = df["Body"]
-print(set(df["Type"])) # 'Phishing', 'Spam', 'Valid' What are the differences between them?
+# print(set(df["Type"])) # 'Phishing', 'Spam', 'Valid' 'Phishing simulation' What are the differences between them?
+print(df.head())
+print(df.sample(10))
+print(df.describe())
+print(df.describe(include="all"))
+
+print(df.dtypes)
+print(df.columns)
+print(df.info())
+
+print(set(df["Type"]))
+
+
+# Data preparation
+
